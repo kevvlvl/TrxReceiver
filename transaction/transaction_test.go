4e4490b1ecf8b5b57ba80d705cc20eb70c84a8dd
Kev <ksayad@gmail.com>
2023-08-30 13:27:25 -0400

    rem boiler plate

--- transaction/transaction_test.go
+++ transaction/transaction_test.go
@@ -2,13 +2,12 @@ package transaction
 
 import (
 	"TrxReceiver/rdb"
-	"bytes"
 	"context"
-	"encoding/json"
 	"github.com/go-redis/redismock/v9"
 	"github.com/redis/go-redis/v9"
 	"github.com/stretchr/testify/assert"
 	"net/http"
+	"strings"
 	"testing"
 )
 
@@ -18,14 +17,13 @@ func TestGetTransaction(t *testing.T) {
 	trx := trxTestSetup(r)
 
 	stock := stockStub()
-	stockBytes, _ := json.Marshal(stock)
 
-	mock.ExpectGet(stock.IdStr()).SetVal(string(stockBytes[:]))
+	mock.ExpectGet(stock.IdStr()).SetVal(stock.AsString())
 
 	resultStock, resultStockBytes := trx.GetTransaction(stock.IdStr())
 
 	assert.Equal(t, stock.Id, resultStock.Id, "The result trx is different from the expected trx")
-	assert.Equal(t, stockBytes, resultStockBytes, "The result trx bytes are different from the expected trx bytes")
+	assert.Equal(t, stock.AsBytes(), resultStockBytes, "The result trx bytes are different from the expected trx bytes")
 }
 
 func TestCreateTransaction(t *testing.T) {
@@ -34,12 +32,11 @@ func TestCreateTransaction(t *testing.T) {
 	trx := trxTestSetup(r)
 
 	stock := stockStub()
-	stockBytes := stock.AsBytes()
 
 	reqUrl := "localhost:4000/trx"
-	req, _ := http.NewRequest("POST", reqUrl, bytes.NewBuffer([]byte(stockRequestBody())))
+	req, _ := http.NewRequest("POST", reqUrl, strings.NewReader(stock.AsString()))
 
-	mock.ExpectSet(stock.IdStr(), string(stockBytes[:]), 0).SetVal("ok")
+	mock.ExpectSet(stock.IdStr(), stock.AsString(), 0).SetVal("ok")
 
 	trx.CreateTransaction(req)
 }
@@ -50,13 +47,12 @@ func TestUpdateTransaction(t *testing.T) {
 	trx := trxTestSetup(r)
 
 	stock := stockStub()
-	stockBytes := stock.AsBytes()
 
 	reqUrl := "localhost:4000/trx/1234"
-	req, _ := http.NewRequest("PUT", reqUrl, bytes.NewBuffer([]byte(stockRequestBody())))
+	req, _ := http.NewRequest("PUT", reqUrl, strings.NewReader(stock.AsString()))
 
-	mock.ExpectGet(stock.IdStr()).SetVal(string(stockBytes[:]))
-	mock.ExpectSet(stock.IdStr(), string(stockBytes[:]), 0).SetVal("ok")
+	mock.ExpectGet(stock.IdStr()).SetVal(stock.AsString())
+	mock.ExpectSet(stock.IdStr(), stock.AsString(), 0).SetVal("ok")
 
 	trx.UpdateTransaction(req, stock.IdStr())
 }
@@ -67,9 +63,8 @@ func TestWriteToRedis(t *testing.T) {
 	trx := trxTestSetup(r)
 
 	stock := stockStub()
-	stockBytes := stock.AsBytes()
 
-	mock.ExpectSet(stock.IdStr(), string(stockBytes[:]), 0).SetVal("ok")
+	mock.ExpectSet(stock.IdStr(), stock.AsString(), 0).SetVal("ok")
 	trx.writeToRedis(stock)
 }
 
@@ -90,8 +85,4 @@ func stockStub() Stock {
 		Name:   "Test Stock",
 		Value:  9001,
 	}
-}
-
-func stockRequestBody() string {
-	return "{\"id\": 1234, \"symbol\": \"TEST\", \"name\": \"Test Stock\", \"Value\": 9001}"
 }
